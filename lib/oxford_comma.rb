917daa6d02a20e169b6cd8503c8dece01a43de19
Donte Ladatto <donte279@gmail.com>
2022-05-27 14:48:56 -0500

    commit tree 6bdccf1f67c21f83e151fbb43abe6411f2983f22

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+    if array.size == 2
+        array.join(" and ")
+    elsif array.size == 1
+        array.join
+    else
+        array.slice(0, array.size - 1).join(", ") << ", and #{array[-1]}"
+    end
 end
